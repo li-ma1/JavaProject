0e624d44478bb9652b8f4a5c7a8fb2512ba01dc3
Lidiya Makarova <li-ma@mail.ru>
2022-03-03 02:44:31 +0300

    3 commit

---
+++ javalesson/src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,50 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+   private static void compareNumbers() {
+        int a = -9, b = 6;
+        if (a>=b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+
+   private static void printColor() {
+       int value = 501;
+       if (value <=0) {
+           System.out.println("Красный");
+       }
+       if (value >0 && value <=100){
+           System.out.println("Желтый");
+       }
+       if (value > 100) {
+           System.out.println("Зеленый");
+       }
+   }
+
+    private static void checkSumSign() {
+        int a = -10, b = 20, c = a+b;
+        if (c>=0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printThreeWords() {
+            System.out.println("Orange\nBanana\nApple");
+    }
+}
+
+
+
